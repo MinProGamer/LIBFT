23d511efb0e6bd2c4612d77d4e5a133de6410e2e
Zakaria Ajaddou <54480895+MinProGamer@users.noreply.github.com>
2024-10-28 17:52:43 +0100

    c

---
+++ ft_atoi.c
@@ -0,0 +1,27 @@
+
+int atoi(const char *str)
+{
+  int result = 0;
+  int sign = 1;
+  int i;
+
+  if(str[i] == '-'){
+    sign = -1;
+    i++;
+  }
+
+  while (/* condition */)
+  {
+    /* code */
+  }
+  
+
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+  int x = atoi("124");
+  return 0;
+}
