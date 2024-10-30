a17d85da1411044602101a789616ae11563f1976
Zakaria Ajaddou <54480895+MinProGamer@users.noreply.github.com>
2024-10-30 01:55:35 +0100

    v

--- work/strncmp.c
+++ work/strncmp.c
@@ -1,12 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   strncmp.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zajaddou <zakariaajaddou@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/28 03:12:52 by zajaddou          #+#    #+#             */
-/*   Updated: 2024/10/28 03:12:53 by zajaddou         ###   ########.fr       */
+/*   Created: 2024/10/29 20:08:16 by zajaddou          #+#    #+#             */
+/*   Updated: 2024/10/30 00:24:52 by zajaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t n){
+  
+  while ((*s1 == *s2) && *s1 && --n)
+    s1++, s2++;
+  
+  return (*s1 - *s2);
+}
