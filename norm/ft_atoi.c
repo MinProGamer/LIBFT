04a6feb9486d18f8442183cca37ef616672340ae
Zakaria Ajaddou <54480895+MinProGamer@users.noreply.github.com>
2024-10-30 23:30:39 +0100

    v

--- norm/ft_atoi.c
+++ norm/ft_atoi.c
@@ -6,41 +6,42 @@
 /*   By: zajaddou <zakariaajaddou@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:57:15 by zajaddou          #+#    #+#             */
-/*   Updated: 2024/10/30 21:41:28 by zajaddou         ###   ########.fr       */
+/*   Updated: 2024/10/30 23:29:36 by zajaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <limits.h>
+#include <stdlib.h>
+
+static void _set_(long *result, int *sign)
+{
+	*result = 0;
+	*sign = 1;
+}
 
 int	ft_atoi(const char *str)
 {
-	int	result;
+	long result;
 	int	sign;
-	int befor	i;
-	int i;
+	long before;
  
-	result = 0;
-	sign = 1;
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
+	_set_(&result, &sign);
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+		if (*str++ == '-')
 			sign = -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (*str >= '0' && *str <= '9')
 	{
-		befor = result;
-		result = (result * 10) + (str[i] - '0');
-		if (result / 10 != befor){
+		before = result;
+		result = (result * 10) + (*str - '0');
+		if ((result / 10) != before)
+		{
 			if(sign == -1)
-				return (0)
-			return (-1)
+				return (0);
+			return (-1);
 		}                               
-		i++;
+		str++;
 	}
 	return (sign * result);
 }
@@ -49,7 +50,11 @@ int	main(void)
 {
 	int	i;
 
-	i = ft_atoi("    +200");
-	printf("%d", i);
-	return (0);
+	char *s = "\n-214748364921474839948";
+
+	i = ft_atoi(s);
+	printf("mine   : %d\n", i);
+
+	i = atoi(s);
+	printf("origin : %d\n", i);
 }
