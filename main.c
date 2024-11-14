d130c15d2023d943ff1716fafdb45dd7715c9b80
Zakaria Ajaddou <54480895+zajaddou@users.noreply.github.com>
2024-11-14 08:26:16 +0100

    c
    c

--- main.c
+++ main.c
@@ -1,24 +1,5 @@
+#define x "\n"
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-
-void add(char **p){
-    *p = "MinPro";
-}
-
-int main(void)
-{
-    int fd = open("data.txt",O_WRONLY | O_CREAT | O_TRUNC,0777);
-
-    char *str = "\nSALAM";
-
-    add(&str);
-
-    int len = strlen(str);
-
-    write(fd,str,len);
-
-    close(fd);
+int main(){
+    x;
 }
